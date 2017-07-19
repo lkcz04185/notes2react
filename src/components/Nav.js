3079d06c8204324cb897bc144ba7e33755edc17c
LK Cody <lkcz04185@gmail.com>
2017-07-19 20:34:57 +0000

    nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render () {
+        return (
+            <div>
+             Navigation Component
+             </div>
+            );
+    }
+    
+    
+}
+
+export default Nav;
